3e622edff5aacf16a1abf6d054cab7fc820aa93f
erichung9060 <rthung96@gmail.com>
2025-11-09 15:15:57 +0800

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import { SharePage } from "./pages/SharePage";
 import { SignPage } from "./pages/SignPage";
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="/sign/:shareId" element={<SignPage />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 };
